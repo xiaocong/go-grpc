cfcefd28e395b7a6e00018cf6c2a865cfa164e87
xiaocong <xiaocong@gmail.com>
2019-05-10 16:22:58 +0800

    add hello rpc

--- go.mod
+++ go.mod
@@ -1,14 +1,20 @@
-module github.com/xiaocong/go-grpc
+module demogrpc
 
 go 1.12
 
 require (
 	cloud.google.com/go v0.38.0 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/gogo/protobuf v1.2.1
+	github.com/golang/protobuf v1.3.1
 	github.com/google/btree v1.0.0 // indirect
 	github.com/google/go-cmp v0.3.0 // indirect
 	github.com/google/pprof v0.0.0-20190404155422-f8f10df84213 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.8.5
 	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/jbenet/goprocess v0.1.0
+	github.com/kisielk/errcheck v1.2.0 // indirect
+	github.com/kr/pty v1.1.4 // indirect
+	github.com/rogpeppe/fastuuid v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20190426145343-a29dc8fdc734 // indirect
 	golang.org/x/exp v0.0.0-20190429183610-475c5042d3f1 // indirect
 	golang.org/x/image v0.0.0-20190501045829-6d32002ffd75 // indirect
@@ -21,7 +27,8 @@ require (
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	golang.org/x/tools v0.0.0-20190501045030-23463209683d // indirect
 	google.golang.org/appengine v1.5.0 // indirect
-	google.golang.org/genproto v0.0.0-20190425155659-357c62f0e4bb // indirect
-	google.golang.org/grpc v1.20.1 // indirect
+	google.golang.org/genproto v0.0.0-20190425155659-357c62f0e4bb
+	google.golang.org/grpc v1.20.1
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 	honnef.co/go/tools v0.0.0-20190418001031-e561f6794a2a // indirect
 )
--- main.go
+++ main.go
@@ -1,4 +1,111 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"demogrpc/hello"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+
+	"github.com/jbenet/goprocess"
+	goprocessctx "github.com/jbenet/goprocess/context"
+	"google.golang.org/grpc"
+)
+
+// implement service
+type helloService struct{}
+
+// Hello method
+func (s *helloService) Hello(ctx context.Context, req *hello.HelloRequest) (*hello.HelloResponse, error) {
+	fmt.Printf("Hello message: %s", req.Message)
+	return &hello.HelloResponse{Code: 1, Message: "Hi! I am working!"}, nil
+}
+
 func main() {
+	parent := goprocess.WithSignals(os.Interrupt)
+	parent.Go(serveRPC)
+
+	select {
+	case <-parent.Closing():
+	}
+
+	select {
+	case <-parent.Closed():
+	}
+}
+
+// 开始 gRPC 服务
+func serveRPC(proc goprocess.Process) {
+	wg := sync.WaitGroup{}
+
+	lis, _ := net.Listen("tcp", "127.0.0.1:10050")
+	s := grpc.NewServer()
+	hello.RegisterSayServer(s, &helloService{})
+
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+
+		fmt.Println("Starting gprc...")
+		_ = s.Serve(lis)
+		go proc.Close()
+	}()
+
+	// start gRPC gateway
+	proc.Go(serveHTTP)
+
+	select {
+	case <-proc.Closing():
+		s.GracefulStop()
+		lis.Close()
+	}
+	wg.Wait()
+}
+
+func serveHTTP(proc goprocess.Process) {
+	// register http gateway handlers
+	// mux := runtime.NewServeMux()
+	// see https://github.com/grpc-ecosystem/grpc-gateway/issues/233
+	mux := runtime.NewServeMux(runtime.WithMarshalerOption(
+		runtime.MIMEWildcard,
+		&runtime.JSONPb{
+			OrigName:     true,
+			EmitDefaults: true,
+		},
+	))
+	wg := sync.WaitGroup{}
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	if err := hello.RegisterSayHandlerFromEndpoint(goprocessctx.OnClosingContext(proc), mux, "127.0.0.1:10050", opts); err != nil {
+		fmt.Printf("error to register endpoint %s", err)
+	}
+
+	var httpendpoint = "127.0.0.1:8080"
+	httpserver := &http.Server{Addr: httpendpoint, Handler: mux}
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+
+		fmt.Println("Starting http...")
+		if err := httpserver.ListenAndServe(); err != http.ErrServerClosed {
+			// close proc only if the err is not ErrServerClosed
+		}
+		go proc.Close()
+	}()
+
+	select {
+	case <-proc.Closing():
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		httpserver.Shutdown(ctx)
+	}
+
+	wg.Wait()
 }
